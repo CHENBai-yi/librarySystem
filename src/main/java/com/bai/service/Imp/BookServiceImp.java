4261121869b223d90690316cd8804e9d2a07c37f
baiyichen <chenbaiyi.work@outlook.com>
2023-11-01 23:40:25 +0800

    bugfix

--- src/main/java/com/bai/service/Imp/BookServiceImp.java
+++ src/main/java/com/bai/service/Imp/BookServiceImp.java
@@ -40,7 +40,7 @@ public class BookServiceImp implements BookService {
 
     public BookServiceImp() {
         if (os_name.contains("linux")) {
-            realPath = "/media";
+            realPath = "/usr/local/tomcat";
         } else if (os_name.contains("windows")) {
             realPath = "D:/tmp";
         }
@@ -141,7 +141,7 @@ public class BookServiceImp implements BookService {
     @Override
     public String uploadBookCoverImg(MultipartFile multipartFile, HttpServletRequest httpServletRequest) throws IOException, RuntimeException {
         // String realPath = httpServletRequest.getServletContext().getRealPath("/");
-        String path = "/static/img/";
+        String path = "/static/img";
         String originalFilename = multipartFile.getOriginalFilename();
         if (StrUtil.isBlank(originalFilename)) {
             log.debug("上传图片名字为空！");
